d7c1e90cd0e7eaf08d06e45c8729ba0d62aaffc0
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-12-12 10:58:03 +0100

    Update

--- app/app.js
+++ app/app.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const url = require('url');
 
 function handler(req, res){
-  res.writeHead(200,{'Content-type':'text/html'});
-  res.write('<h1>Hello, I am a webserver!</h1>');
-  res.end();
+
+  const parsedUrl = url.parse(req.url, true);
+  //console.log(parsedUrl);
+
+
+  if(parsedUrl.pathname ==='/'){
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.write('<h1>Hello, I am a webserver!</h1>');
+    res.end();
+  } else{
+    res.writeHead(404,{'Content-type':'text/html'});
+    res.end();
+
+  }
+
+
 }
 
 const server = http.createServer(handler);
